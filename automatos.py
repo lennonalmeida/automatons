94f4b80d4ce944eb04120e240607823d7ee5fcb5
lennonalmeida <lennonraribeiro@gmail.com>
2018-05-28 22:30:12 -0300

    words with initial letters A D T done

--- automatos.py
+++ automatos.py
@@ -1,28 +1,85 @@
 
 def state(word):
     #se nao formar palavra state = This word dont belong to our alphabet
-    #
+    reject = 'This word dont belong to our alphabet'
+    accepted = ' | Accepted'
     first = word[0:1]
     if first == 'f':
-        #or
-    elif first == 'o':
-        #r
-    elif first == 'i':
-        #s or n
-    elif first == 'n':
-        #ot
+        state = 'hell'
+    #     #or
+    # elif first == 'o':
+    #     #r
+    # elif first == 'i':
+    #     #s or n
+    # elif first == 'n':
+    #     #ot
     elif first == 'a':
         #s or nd
+        state = 'q0->q13'
+        if word[1:2] == 's':
+            state += '->q3'
+            if len(word)>2:
+                state = reject
+            else:
+                state+=accepted
+        elif word[1:2] == 'n':
+            state =  'q0->q1->q2'
+            if word[2:3] == 'd':
+                state += '->q3'
+                if len(word) > 3:
+                    state = reject
+                else:
+                    state+=accepted
+            else:
+                state+=' | '+reject
+        else:
+            state += ' | '+reject
+
     elif first == 'd':
         #ef or el
+        state = 'q0->q6'
+        if word[1:2] == 'e':
+            if word[2:3] == 'f':
+                if len(word) > 3:
+                    state = reject
+                else:
+                    state += '->q7->q3' + accepted
+            elif word[2:3] == 'l':
+                if len(word) > 3:
+                    state = reject
+                else:
+                    state += '->q4->q3' + accepted
+            else:
+                state +=' | ' + reject
+        else:
+            state = reject
     elif first == 't':
-        #ry
+         #ry
+         if word[1:2] == 'r':
+             state = 'q0->q10->q11'
+             if word[2:3] == 'y':
+                 state += '->q3'
+                 if len(word)>3:
+                     state = reject
+                 else:
+                    state+=accepted
+             else:
+                 state += ' | '+reject
+         else:
+             state = reject
     else:
-        state = 'This word dont belong to our alphabet'
+        state = reject
 
     return state
 
 #main code
-wd = input("Word to be verfied:")
-state_out = state(wd)
-print(state_out)
+print("Press CTRL+C to interrupt the program")
+while True:
+    try:
+        wd = input("Word to be verfied:")
+        state_out = state(wd)
+        print(state_out)
+        print("-------------------------")
+    except KeyboardInterrupt:
+        print("CTRL-C pressed")
+        break
